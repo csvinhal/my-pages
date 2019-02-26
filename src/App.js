45cfc029a6a41d75e7dbca7b491db190b0d3f8f0
csvinhal <cristiano.vinhal10@gmail.com>
2019-02-25 21:22:20 -0300

    profile-header start

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import './App.css';
+import './App.scss';
 import ProfileHeader from './components/profile-header/profile-header';
 
 class App extends Component {
