f99e04ca254eca1c9a7b49e972c25c4ad28fe08d
csvinhal <cristiano.vinhal10@gmail.com>
2020-02-12 22:59:42 -0300

    Updates

---
+++ precache-manifest.8530ece473160f8f872b53d804956964.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "01b534292b205ada61d4aac1088bfa10",
+    "url": "/my-pages/index.html"
+  },
+  {
+    "revision": "d5026235020cf69303b1",
+    "url": "/my-pages/static/css/main.f31a905c.chunk.css"
+  },
+  {
+    "revision": "628cb0cd60f8210b93ef",
+    "url": "/my-pages/static/js/2.252a139a.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/my-pages/static/js/2.252a139a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d5026235020cf69303b1",
+    "url": "/my-pages/static/js/main.f6dd9981.chunk.js"
+  },
+  {
+    "revision": "058c08e8af51e73f8d6d",
+    "url": "/my-pages/static/js/runtime-main.7a977c55.js"
+  },
+  {
+    "revision": "d283cad9252413834ba60c6f6a0263dc",
+    "url": "/my-pages/static/media/profile.d283cad9.JPG"
+  },
+  {
+    "revision": "4dc07cdf4f9724b16a41eab7809f560d",
+    "url": "/my-pages/static/media/toolbar_background.4dc07cdf.jpg"
+  }
+]);
