c225cf8ed3e7552fa0dd5292dfb902a8675be9dc
csvinhal <cristiano.vinhal10@gmail.com>
2020-02-16 15:38:18 -0300

    updated about me

--- src/screen/About/About.js
+++ src/screen/About/About.js
@@ -11,16 +11,18 @@ const About = () => {
     <div className="about">
       <Title title="About me" />
       <Card>
-        <CardContainer>
-          <div className="card-container__text">
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur
-            non mattis elit. Vestibulum a sodales nulla. Ut id sem a est ornare
-            placerat. Suspendisse non odio accumsan, rhoncus felis id, ornare
-            urna. Sed id pharetra risus. Cras sem magna, sodales vitae lectus
-            at, varius scelerisque sem. Nam commodo, ipsum ac dignissim pretium,
-            neque sapien ultricies augue, at porttitor massa augue id sapien.
-            Praesent semper elit a aliquet maximus.
-          </div>
+        <CardContainer className="card-container__text">
+          <p>
+            First and foremost, I love coding, creating and optimizing
+            interactive, user-friendly websites/apps bringing a wonderful
+            experience and including people.
+          </p>
+          <p>
+            My specialties include quickly learning new skills and programming
+            languages, problem solving, responsive design principles and strong
+            attention to detail in order to deliver original and efficient
+            solutions.
+          </p>
         </CardContainer>
         <CardFooter className="about__footer">
           <Button label="Download resume" priority={"primary"} />
